42eadfdfc7b17ab70e18add82a65c636b833cc83
BlackTrub <lightlocdog@gmail.com>
2016-11-21 14:21:47 +0300

    modify test.py;

--- test.py
+++ test.py
@@ -5,6 +5,7 @@
 # password = 'password'
 # test_user_id = 0000000
 # test_user_post_id = 0000000
+from pprint import pprint
 
 from InstagramAPI import InstagramAPI
 from constants import login, password, test_user_id, test_user_post_id
@@ -18,19 +19,23 @@ api.unfollow(test_user_id)
 api.like(test_user_post_id)
 api.unlike(test_user_post_id)
 
+print '## Followers ##'
 api.getUserFollowers(test_user_id)
-print api.LastJson
+pprint(api.LastJson)
 
+print '## Following ##'
 api.getUserFollowings(test_user_id)
-print api.LastJson
+pprint(api.LastJson)
 
 api.comment(test_user_post_id, 'test comment')
 
+print '## Media likers ##'
 api.getMediaLikers(test_user_post_id)
-print api.LastJso
+pprint(api.LastJson)
 
+print '## Geo media ##'
 api.getGeoMedia(test_user_id)
-print api.LastJson
+pprint(api.LastJson)
 
-api.uploadPhoto('12345.jpg', caption='test load photo')
+api.uploadPhoto('test_photo.jpg', caption='test load photo')
 
